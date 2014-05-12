394272f42e582920248c2c8f39114bda2ba493ca
Nik Daftary <nikdaftary@gmail.com>
2014-05-11 17:24:21 -0700

    added new page 'About Us' w/ passing test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,31 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+# -------  HOME PAGE TEST -------
   describe "Home page" do
-
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
   end
+
+# -------  HELP PAGE TEST -------
+    describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+# -------  ABOUT US PAGE TEST -------
+    describe "About Us page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+
+
+
+
 end
