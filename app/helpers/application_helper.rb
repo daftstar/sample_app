df8321b347fb987eb70871e3560695d219e5623b
Nik Daftary <nikdaftary@gmail.com>
2014-05-24 12:49:13 -0500

    adding helper method for base title check

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  #Returns the full title on a per page basis
+  def full_title(page_title)
+    # if a title on the view page exists, then use base title + "|" + title
+    # otherwise only use the base title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      # "#{base_title} (this page has a title) | #{page_title}"
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
